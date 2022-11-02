d3987a3feaacb34718e20b4b9704aa6461a2ffbd
apuente <angel.l.puente@gmail.com>
2022-11-02 13:12:38 -0500

    added first modal, updated some css

---
+++ Portfolio_Items/app.js
@@ -0,0 +1,17 @@
+console.log("test")
+const openModal = document.getElementById('modalOn')
+
+const modal = document.getElementById('modal')
+const exitModal = document.getElementById('close')
+
+function startModal(){
+    modal.style.display = 'flex'
+}
+
+function closeModal(){
+    modal.style.display= 'none'
+}
+openModal.addEventListener('click', startModal)
+exitModal.addEventListener('click', closeModal)
+
+setTimeout(startModal, 7000)
